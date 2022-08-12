e83a86e43e511e3801d7961000a1d37f31ab71a8
tianhouju <tianhouju@360.cn>
2022-08-12 19:36:20 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
-  <div>
-    <!-- <t-button :tDisabled="state" >
-    按钮
-  </t-button> -->
-    <!-- <t-cascader></t-cascader> -->
-    <!-- <button @click="handle">test</button> -->
-    <!-- <t-rate></t-rate> -->
-    <t-slider></t-slider>
+  <div class="test">
+    <div>test</div>
+    <div><span>红包个数</span> <input type="text"  v-model="count"/>个</div>
+    <div><span>总   金   额</span> <input type="text" v-model="money"/>元</div>
+    <button @click="start">发红包</button>
+
+    <div >
+
+    </div>
   </div>
 </template>
 <script setup>
-import { ref } from "vue";
-let state = ref(true);
-const handle = () => {
-  state.value = !state.value;
-  console.log(state.value);
-};
+import {ref} from 'vue'
+const money=ref(0)
+const count=ref(0)
+const start=()=>{
+  const moneyArr=[]
+  let restmoney=money
+  while(moneyArr.length<count.value){
+         let  averageMoney=restmoney/(count.value-money.length)
+        //  let rmoney=Math.random()*
+  }
+}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.test {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+  height: 500px;
+  border: 1px solid orange;
+}
+</style>
--- src/components/Slider/index.vue
+++ src/components/Slider/index.vue
@@ -1,34 +1,88 @@
 <template>
-  <div class="slider">
-    <div class="slider_line"></div>
-    <div class="slider_btn"></div>
+  <div class="slider" @mouseup="cleanmove">
+    <div class="slider_line" @mousedown="move"></div>
+    <div
+      class="slider_btn"
+      @mouseenter="start"
+      @mouseleave="clean"
+      @mousedown="movebtn"
+      @mouseup="cleanmove"
+    ></div>
+    <div class="slider_num">{{num}}</div>
   </div>
 </template>
 
-<script></script>
+<script setup>
+import { ref } from "vue";
+const isActived = ref(false);
+const offsetLeft = ref(0);
+const num=ref(0)
+const start = () => {
+  isActived.value = true;
+};
+const clean = () => {
+  isActived.value = false;
+};
+const move = (e) => {
+  if (e.offsetX < 10) {
+    offsetLeft.value = 0;
+  } else {
+    offsetLeft.value = e.clientX - 15 + "px";
+  }
+
+};
+const movebtn = (e) => {
+  const line=document.querySelector('.slider_line')
+  offsetLeft.value = e.clientX - 15 + "px";
+  window.addEventListener("mousemove", move);
+  // num.value=
+  console.log(line.style.width);
+};
+const cleanmove = () => {
+  window.removeEventListener("mousemove", move);
+};
+</script>
 
 <style lang="scss" scoped>
 .slider {
-    display: flex;
-    align-items: center;
-    position: relative;
-    cursor: pointer;
-    height: 30px;
+  display: flex;
+  align-items: center;
+  // justify-content: center;
+  position: relative;
+  cursor: pointer;
+  height: 50px;
+
   &_line {
     width: 600px;
     height: 8px;
-    background-color: #589ff8;
+    background-color: gray;
     border-radius: 4px;
   }
-  &_btn{
+
+  &_btn {
     position: absolute;
-    left: 0;
-    top: 0;
-    width: 28px;
-    height: 28px;
+    left: v-bind(offsetLeft);
+    width: v-bind("isActived?'35px':'28px'");
+    height: v-bind("isActived?'35px':'28px'");
     border-radius: 50%;
-    border:1px solid #589ff8;
+    border: 1px solid #589ff8;
     background-color: white;
+    transition: 1s;
+
+    &:hover {
+      width: 35px;
+      height: 35px;
+    }
+  }
+
+  &_num {
+    // display: v-bind("isActived?'block':'none'");
+    width: 20px;
+    height: 20px;
+    background-color: black;
+    color: aliceblue;
+    line-height: 20px;
+    text-align: center;
   }
 }
 </style>
